35f1237c6f397ca323bb221fb4a4dd8b554ec3dd
Domino Valdano <domino@apache.org>
2021-07-21 21:58:55 +0000

    Change default depth to num_segs instead of log_2(num_segs)
    log_2(num_segs) made sense for the kd_tree, because each node of the tree
    is always split exactly in half, resulting in a balanced binary tree.
    With the optimized tree algorithm, splits can happen anywhere, there can
    be 1 segment on the left and 50 on the right, and then later down the
    tree the 50 get split up more while the 1 does not.  In the worst case,
    one node gets split off from the rest at each cut, meaning in order to
    populate num_segs leaves you need num_segs cuts.  This could result in
    a longer optimzation time, but the optimization phase is usually very
    fast compared to the actual parallel-DBSCAN phase that follows.
    It should only matter for small datasets, and the user always has the
    option of setting a lower max_depth than the default if optimization
    is taking too much time.

---
+++ src/ports/postgres/modules/dbscan/test/perf/run_64d_23clusters.sql
@@ -0,0 +1,4 @@
+\set MADLIB_PERF_DIR '/home/':USER'/madlib/src/ports/postgres/modules/dbscan/test/perf/blobs/'
+DROP TABLE IF EXISTS output_blobs_64d_23c_003s_100000p,output_blobs_64d_23c_003s_100000p_summary;
+SELECT madlib.dbscan('input_blobs_64d_23c_003s_100000p', 'output_blobs_64d_23c_003s_100000p', 'id', 'point', 0.0285, 3, 'dist_norm2', 'kd_tree');
+COPY output_blobs_64d_23c_003s_100000p TO :'MADLIB_PERF_DIR''output_blobs_64d_23c_003s_100000p.csv' CSV HEADER;
